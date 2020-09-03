c0e07ad13feed0116bbd0d09754477e583d603a0
Dennis Kimani <denoh6804@gmail.com>
2020-09-03 21:03:45 +0300

    Setting up the viewPager and Tablayout with TabLayoutMediator

--- app/src/main/java/com/example/gad2020/MainActivity.kt
+++ app/src/main/java/com/example/gad2020/MainActivity.kt
@@ -2,12 +2,28 @@ package com.example.gad2020
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        //SETTING UP THE VIEWPAGER
+
+        val pageAdapter = PageAdapter(this)
+        viewPager.adapter = pageAdapter
+        //SETTING UP TABLAYOUTMEDIATOR TO LINK UP THE TABLAYOUT WITH VIEWPAGER
+        TabLayoutMediator(tab_layout, viewPager) { tabs, position ->
+            if (position == 0) {
+                tabs.text = "LEARNING LEADERS"
+            } else {
+                tabs.text = "SKILL IQ LEADERS"
+            }
+        }.attach()
     }
+
 }
